f0eff7d20face705bdc26efbd7d9c995b772f9d9
Sweetsan <coderintheworldhack100@gmail.com>
2026-05-18 13:17:12 +0530

    modified

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Main py running ...")
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "mysql+pymysql://root:root123@localhost/uki_school"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+@app.route("/")
+def home():
+    return "Flask + MySQL Connected"
+
+
+if __name__ == "__main__":
+    try:
+        with app.app_context():
+            db.session.execute(text("SELECT 1"))
+            print("SUCCESS: Database Connected Successfully")
+
+    except Exception as e:
+        print("ERROR: Database Connection Failed")
+        print(e)
+
+    app.run(debug=True)
